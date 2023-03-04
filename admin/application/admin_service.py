d65560054069da08ac6f900705ee1bc179c7df05
Francisco RV <lmethod1234@gmail.com>
2023-03-04 14:21:02 -0600

    refactor: add exceptions to admin_service when a process goes wrong and update adminapi_fastpi to handle those exceptions

--- admin/application/admin_service.py
+++ admin/application/admin_service.py
@@ -5,31 +5,38 @@ class AdminService:
     def __init__(self, admin_repository: ISuperAdminRepository):
         self.__admin_repository = admin_repository
 
-    def create_admin(self, new_admin: Admin) -> Admin:
+    def create_admin(self, new_admin: Admin) -> AdminRepositoryException | bool:
+        if isinstance(self.get_admin_by_id("email", new_admin.email), Admin):
+            raise AdminRepositoryException(f"Admin with id {new_admin.email} already in db")
         result = self.__admin_repository.create(new_admin)
         if result:
-            return new_admin
+            return result
         raise AdminRepositoryException("Admin could not be created in db")
     
     def get_admin_by_id(self, field: str, admin_id: str) -> Admin:
         admin = self.__admin_repository.get_by_id(field, admin_id)
-        if admin:
-            return admin
-        raise AdminRepositoryException(f"Admin with id {admin_id} not found")
+        return admin if admin else None
     
     def get_all_admins(self) -> list[Admin]:
         return self.__admin_repository.get_all()
     
-    def update_admin(self, admin: Admin) -> Admin:
+    def update_admin(self, admin: Admin) -> bool:
+        # If the admin already has the values we are trying to update, we return True directly
+        current_admin = self.get_admin_by_id("email", admin.email)
+        if (current_admin is not None and current_admin.username == admin.username and
+            current_admin.password == admin.password):
+            raise AdminRepositoryException(f"Admin with id {current_admin.email} already updated")
         admin.is_superadmin = False
         result = self.__admin_repository.update(admin)
         if result:
-            return admin
-        raise AdminRepositoryException(f"Admin with mail {admin.email} not found")
+            return result
+        raise AdminRepositoryException(
+            f"Admin with id {current_admin.email} could not be updated")
     
     def delete_admin(self, field: str, admin_id: str) -> bool:
         result = self.__admin_repository.delete(field, admin_id)
         if result:
-            return True
-        raise AdminRepositoryException(f"Admin with {field} {admin_id} not found")
+            return result
+        raise AdminRepositoryException(
+            f"Admin with {field} {admin_id} not found")
 
--- admin/infrastructure/adminapi_fastapi.py
+++ admin/infrastructure/adminapi_fastapi.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, HTTPException, status
 from admin.domain.admin_model import Admin
 from admin.application.superadmin_repository import ISuperAdminRepository, AdminRepositoryException
-from admin.infrastructure.fakedb_superadmin_repository import FakeDBSuperAdminRepository
+from admin.infrastructure.mongodb_admin_repository import MongoDBSuperAdminRepository
 from admin.application.admin_service import AdminService
-#from admin.infrastructure.mongodb_admin_repository import MongoDBSuperAdminRepository
+#from admin.infrastructure.fakedb_superadmin_repository import FakeDBSuperAdminRepository
 
 
 router = APIRouter(prefix="/admin",
@@ -11,28 +11,30 @@ router = APIRouter(prefix="/admin",
                    responses={status.HTTP_404_NOT_FOUND: {"message": "Not found"}})
 
 # Setup admin service with MongoDBAdminRepository
-#admin_repo: ISuperAdminRepository = MongoDBSuperAdminRepository("MONGO_URI")
-admin_repo: ISuperAdminRepository = FakeDBSuperAdminRepository()
+admin_repo: ISuperAdminRepository = MongoDBSuperAdminRepository("MONGO_URI")
+#admin_repo: ISuperAdminRepository = FakeDBSuperAdminRepository()
 admin_service = AdminService(admin_repo)
 
 
-@router.post("/", response_model=Admin, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=Admin)
 async def create_admin(admin: Admin):
     try:
-        if isinstance(admin_service.get_admin_by_id("email", admin.email), Admin):
+        result = admin_service.create_admin(admin)
+        if result:
             raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND, detail="User already exists")
-        created_admin = admin_service.create_admin(admin)
-        return created_admin
-    except AdminRepositoryException as exc:
+                status_code=status.HTTP_201_CREATED, 
+                detail="User created")
+    except AdminRepositoryException as ex:
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(exc)) from exc
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"{ex}"
+        ) from ex
+    
 
 @router.get("/{admin_id}")
 async def get_admin(admin_id: str) -> Admin:
     try:
         admin = admin_service.get_admin_by_id("email", admin_id)
-        print(f"adminfound\n {admin}")
         if admin is None:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND, detail="Admin not found")
@@ -47,25 +49,31 @@ async def get_all_admins() -> list[Admin]:
         admins = admin_service.get_all_admins()
     except AdminRepositoryException as exc:
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(exc)) from exc
+            status_code=status.HTTP_404_NOT_FOUND, detail=str(exc)) from exc
     return admins
 
 
 @router.put("/{admin_id}")
 async def update_admin(admin: Admin):
-    updated_admin = admin_service.update_admin(admin)
-    if updated_admin is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Admin not found")
-    return updated_admin
+    try:
+        result = admin_service.update_admin(admin)
+        if result:
+            raise HTTPException(
+                status_code=status.HTTP_200_OK,
+                detail=f"Admin with email {admin.email} updated from db")
+    except AdminRepositoryException as ex:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail=str(ex)) from ex
 
 
 @router.delete("/{admin_id}")
 async def delete_admin(admin_id: str):
     try:
-        deleted_admin = admin_service.delete_admin("email", admin_id)
-        if not deleted_admin:
+        result = admin_service.delete_admin("email", admin_id)
+        if result:
             raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND, detail="Admin not found")
+                status_code=status.HTTP_200_OK,
+                detail=f"Admin with id {admin_id} deleted from db")
     except AdminRepositoryException as exc:
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(exc)) from exc
+            status_code=status.HTTP_409_CONFLICT, detail=str(exc)) from exc
