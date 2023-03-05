d95a1742f494f8bb586529e59fe69749d94f2a1b
Francisco RV <lmethod1234@gmail.com>
2023-03-04 23:12:37 -0600

    chore: update product diagram and main

--- admin/main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-import admin.infrastructure.adminapi_fastapi as adminapi
-app = FastAPI()
-
-app.include_router(adminapi.router)
--- fastapi_main.py
+++ fastapi_main.py
@@ -1,14 +1,7 @@
 from fastapi import FastAPI
-
-# Inicia el server: python -m uvicorn fastapi_main:app --reload
-
-# pip install --target=D:\Python package_name --no-user
-
+import admin.infrastructure.adminapi_fastapi as adminapi
+import product.infrastructure.productapi_fastapi as productapi
 app = FastAPI()
 
-# Routers
-#app.include_router(products.router)
-#app.include_router(users.router)
-#app.include_router(jwt_auth_users.router)
-#app.include_router(users_db.router)
-
+app.include_router(adminapi.router)
+app.include_router(productapi.router)
